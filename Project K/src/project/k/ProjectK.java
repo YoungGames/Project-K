4da7c034ca3a75c25e1f392f90695559787302d6
joshi1999 <joshi1999@yahoo.de>
2015-07-08 20:40:20 +0200

    Example

--- Project K/src/project/k/ProjectK.java
+++ Project K/src/project/k/ProjectK.java
@@ -1,6 +1,9 @@
 
 package project.k;
 
+import java.net.URL;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JButton;
@@ -15,6 +18,11 @@ public class ProjectK {
         startframe.setSize(200, 200);
         startframe.setResizable(false);
         startframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        //URL firstpicturename = ProjectK.class.getResource("/project/k/textures/Picturename.png");
+        //Icon secondpicturename = new ImageIcon(firstpicturename);
+        
+        
+        
     }
-    
 }
