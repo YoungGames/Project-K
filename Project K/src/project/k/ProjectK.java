f9549e05e2b3033d0202b133e56671529559cccd
joshi1999 <joshi1999@yahoo.de>
2015-07-08 20:33:08 +0200

    Startframe

--- Project K/src/project/k/ProjectK.java
+++ Project K/src/project/k/ProjectK.java
@@ -1,12 +1,20 @@
 
 package project.k;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class ProjectK {
 
     
     public static void main(String[] args) {
-       
+        JFrame startframe = new JFrame("Project K");
+        startframe.setLayout(null);
+        startframe.setVisible(true);
+        startframe.setSize(200, 200);
+        startframe.setResizable(false);
+        startframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
 }
