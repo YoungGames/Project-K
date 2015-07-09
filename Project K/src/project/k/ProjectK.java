a6ff8c17ac4820f00ee6551a83cc6bcf58d5afc6
joshi1999 <joshi1999@yahoo.de>
2015-07-09 22:02:39 +0200

    Add color

--- Project K/src/project/k/ProjectK.java
+++ Project K/src/project/k/ProjectK.java
@@ -1,6 +1,9 @@
 
 package project.k;
 
+import java.net.URL;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -17,7 +20,10 @@ public class ProjectK {
         
         //URL firstpicturename = ProjectK.class.getResource("/textures/Picturename.png");
         //Icon secondpicturename = new ImageIcon(firstpicturename);
-        
+        URL namesave_ = ProjectK.class.getResource("/textures/namesave.png");
+        URL backgroundtexture_ = ProjectK.class.getResource("/textures/backgroundtexture.png");
+        Icon namesave = new ImageIcon(namesave_);
+        Icon backgroundpicture = new ImageIcon(backgroundtexture_);
         
         
         int newgame = 1;
@@ -35,20 +41,28 @@ public class ProjectK {
             JTextField name = new JTextField();
             name.setSize(200, 20);
             name.setLocation(100, 180);
-        
+            
+            JLabel backgroundtexture = new JLabel();
+            backgroundtexture.setSize(400, 400);
+            backgroundtexture.setIcon(backgroundpicture);
+            
             JLabel Name = new JLabel();
             Name.setSize(50, 20);
             Name.setText("Name:");
             Name.setLocation(50, 180);
             
-            JButton save = new JButton("Save");
+            JButton save = new JButton();
             save.setSize(250, 30);
             save.setLocation(50, 201);
+            save.setIcon(namesave);
             
             startframe.setSize(400, 400);
+            
             startframe.add(name);
             startframe.add(Name);
             startframe.add(save);
+            startframe.add(backgroundtexture);
+            backgroundtexture.setVisible(true);
             name.setVisible(true);
             Name.setVisible(true);
             save.setVisible(true);
@@ -60,6 +74,7 @@ public class ProjectK {
                     startframe.setVisible(false);
                     name.setVisible(false);
                     Name.setVisible(false);
+                    save.setVisible(false);
                     break;
                 }
             }   
