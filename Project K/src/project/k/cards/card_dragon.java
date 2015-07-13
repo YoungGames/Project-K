45e6bcf9474f0177af206402ff52532048b81e54
joshi1999 <joshi1999@yahoo.de>
2015-07-13 21:53:10 +0200

    card addition

--- Project K/src/project/k/cards/card_dragon.java
+++ Project K/src/project/k/cards/card_dragon.java
@@ -2,17 +2,20 @@ package project.k.cards;
 
 import javax.swing.JLabel;
 
+
 public class card_dragon {
     
     public int lvl = 1;
     public int xp = 0;
     public double live = 100;
+    public double max_live = 100;
     public double attack = 20;
     public double shield = 5;
     
     public static void dragon() {
         JLabel dragon = new JLabel();
         dragon.setSize(20, 50);
+        
     }
 
     public void setLevel(int i) {
@@ -24,9 +27,20 @@ public class card_dragon {
         if(xp > 10 && lvl < 2) {
             lvl = 2;
             live = live + (live*0.1);
+            max_live = live;
         }
         else if(xp > 100 && lvl < 3) {
             lvl = 3;
+            live = live + (live*0.1);
+            max_live = live;
         }
+    }
+    
+    public void removeLive(int i) {
+        live = live - i;
+    }
+    
+    public void setMaxLive() {
+        live = max_live;
     }
 }
